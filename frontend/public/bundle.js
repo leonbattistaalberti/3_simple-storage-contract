321be8cf441518fc8ac77afc98f408608e4435fe
thelonehegelian <thelonehegelian@gmail.com>
2020-08-17 07:44:04 +0100

    Migrate contracts and add abi and address to bundle.js

--- frontend/public/bundle.js
+++ frontend/public/bundle.js
@@ -1,7 +1,40 @@
-var simpleStorageABI = [];
-var simpleStorageAddress = '';
-var web3 = new Web3('http://localhost:9545');
-var simpleStorage = new web3.eth.Contract(simpleStorageABI, simpleStorageAddress);
+var simpleStorageABI = [
+  {
+    constant: false,
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "x",
+        type: "uint256",
+      },
+    ],
+    name: "set",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "get",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+];
+var simpleStorageAddress = "0xD0382378208E60Ef6433bAc0bA1d03AED6E3d1f1";
+var web3 = new Web3("http://localhost:9545");
+var simpleStorage = new web3.eth.Contract(
+  simpleStorageABI,
+  simpleStorageAddress
+);
 
-document.addEventListener('DOMContentLoaded', () => {
-});
+document.addEventListener("DOMContentLoaded", () => {});
